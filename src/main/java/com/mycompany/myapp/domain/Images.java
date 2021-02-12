c0eb4301ed4d1568c4cd14308866842ffb7f11ca
Théo <theo.mezier@gmail.com>
2021-02-12 17:16:57 +0100

    database mise à jour

--- src/main/java/com/mycompany/myapp/domain/Images.java
+++ src/main/java/com/mycompany/myapp/domain/Images.java
@@ -1,11 +1,17 @@
 package com.mycompany.myapp.domain;
 
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
-
-import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * A Images.
